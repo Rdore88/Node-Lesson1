5c4bd25a72bacb1e7bad304e0bebf4766ea258b9
Isaac Hardy <isaac@ihardy.co>
2017-06-19 11:36:33 -0400

    hard mode

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ app.get('/home', function(req, res){
   res.sendFile('index.html' , { root : __dirname});
 });
 
-app.get('/hello', function(req, res){
-  res.sendFile('hello.html', { root : __dirname});
+app.get('/hello/:name', function(req, res){
+  res.send("Hello, there " + req.params.name + "!");
 });
 
 app.listen(3000, function (){
