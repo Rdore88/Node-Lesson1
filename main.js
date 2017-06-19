cb92882a4a987ffd61911681dda289436b09f94d
Isaac Hardy <isaac@ihardy.co>
2017-06-18 22:43:11 -0400

    updated - isaac

--- main.js
+++ main.js
@@ -2,15 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use('/css', express.static('css'));
-
+app.use(express.static(path.join(__dirname, 'css')))
 
 app.get('/home', function(req, res){
-  res.sendFile('index.html', { root: '.' });
+  res.sendFile('index.html' , { root : __dirname});
 });
 
 app.get('/hello', function(req, res){
-  res.sendFile('hello.html', { root: '.' });
+  res.sendFile('hello.html', { root : __dirname});
 });
 
 app.listen(3000, function (){
