aa492aaeee348288888f40aff8d0ae20f2d15361
Isaac Hardy <isaac@ihardy.co>
2017-06-18 22:58:24 -0400

    moved css to public; aded public to static call

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static('public'));
 
 app.get('/home', function(req, res){
   res.sendFile('index.html' , { root : __dirname});
